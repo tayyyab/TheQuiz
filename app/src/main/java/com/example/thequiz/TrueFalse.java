aa49126290f5842d8067f23b396bc8acce318955
tayyab <mtbahauddin@hotmail.com>
2019-08-01 17:30:12 +0500

    Model: Create TrueFalse model

---
+++ app/src/main/java/com/example/thequiz/TrueFalse.java
@@ -0,0 +1,28 @@
+package com.example.thequiz;
+
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    private TrueFalse(int question, boolean trueQuestion){
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
